5a8e7b929f04428d4122fb4286d9ddbb0354331f
jungledj <junglaq@hotmail.com>
2019-03-22 20:14:56 +0000

    Internacinalizacion Utilizacion del archivo i18n

--- MyApp/webapp/controller/App.controller.js
+++ MyApp/webapp/controller/App.controller.js
@@ -5,8 +5,18 @@ sap.ui.define(
 	], function (Controller, MessageToast){
 		"use strict";
 		return Controller.extend("opensap.myapp.controller.App", {
-				onShowHello: function(){
-				MessageToast.show("Hola inMundirijillo!!");
+			onShowHello: function(){
+			
+			//	MessageToast.show("Hola inMundirijillo!!")
+			//  >> Se pone el siguiente código para utilizar el archivo i18n
+			
+			// read msg from i18n model:
+			var oBundle = this.getView().getModel("i18n").getResourceBundle();
+			var sRecipient = this.getView().getModel("helloPanel").getProperty("/recipient/name");
+			var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+
+			// show message
+			MessageToast.show(sMsg);
 			}	
 		});
 	});
